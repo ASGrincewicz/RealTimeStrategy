9a19ac34f10a0fc53bb0a86bc10a96b64fde1b37
Aaron Grincewicz <asgrincewicz@icloud.com>
2021-09-28 13:52:51 -0400

    Units chasing after targeted unit until target cleared.

--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -6,11 +6,27 @@ public class UnitMovement : NetworkBehaviour
 {
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField] private Targeter _targeter;
+    [SerializeField] private float _chaseRange = 10f;
 
     #region Server
     [ServerCallback]
     private void Update()
     {
+        Targetable target = _targeter.GetTarget();
+
+        if(_targeter.GetTarget() != null)
+        {
+            if((target.transform.position - transform.position).sqrMagnitude > _chaseRange * _chaseRange)
+            {
+                _navMeshAgent.SetDestination(target.transform.position);
+            }
+            else if(_navMeshAgent.hasPath)
+            {
+                _navMeshAgent.ResetPath();
+            }
+            return;
+        }
+
         if (!_navMeshAgent.hasPath) return;
         if (_navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance) return;
 
@@ -25,7 +41,6 @@ public class UnitMovement : NetworkBehaviour
         _navMeshAgent.SetDestination(hit.position);
 
         _targeter.ClearTarget();
-
     }
     #endregion
 }
