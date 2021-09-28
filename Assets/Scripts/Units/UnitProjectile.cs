35b0a01b8a7642480982230e75a810bc908b49fc
Aaron Grincewicz <asgrincewicz@icloud.com>
2021-09-28 18:20:41 -0400

    Units and buildings can have Health component and take and/or deal damage.

--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -9,9 +9,27 @@ public class UnitProjectile : NetworkBehaviour
 
     [SerializeField] private float _launchForce = 10f;
 
+    [SerializeField] private int _damageToDeal = 20;
+
     private void Start() => _rigidBody.velocity = transform.forward * _launchForce;
 
     public override void OnStartServer() => Invoke(nameof(DestroySelf), _destroyAfterSeconds);
+
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) return;
+        }
+
+        if(other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(_damageToDeal);
+        }
+        DestroySelf();
+    }
 
     [Server]
     private void DestroySelf() => NetworkServer.Destroy(this.gameObject);
